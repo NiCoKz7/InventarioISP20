e35f05a5683c2da5e4ac8a484660207423b5d744
NiCoKz7 <nicomarquezokk5@gmail.com>
2026-09-16 23:13:24 -0300

    Actualiza conexión y migración de datos semillas Clientes
    Se cambió la cadena de conexión a "postgreLocal" en InventarioContext.cs y Program.cs.
    Se generó la migración "20260917021158_localidades_controllers" que actualiza los valores de Created_at para los Clientes con Id 1, 2 y 3.
    Se actualizó InventarioContextModelSnapshot.cs y el archivo Designer de la migración para reflejar los nuevos datos semillas y el modelo actualizado de la base de datos.

--- Backend/Migrations/20260917021158_localidades_controllers.Designer.cs
+++ Backend/Migrations/20260917021158_localidades_controllers.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Backend.DBContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Backend.Migrations
 {
     [DbContext(typeof(InventarioContext))]
-    partial class InventarioContextModelSnapshot : ModelSnapshot
+    [Migration("20260917021158_localidades_controllers")]
+    partial class localidades_controllers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend/Migrations/20260917021158_localidades_controllers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class localidades_controllers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Clientes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 16, 23, 11, 56, 394, DateTimeKind.Unspecified).AddTicks(2765), new TimeSpan(0, -3, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Clientes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 16, 23, 11, 56, 394, DateTimeKind.Unspecified).AddTicks(2841), new TimeSpan(0, -3, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Clientes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 17, 2, 11, 56, 394, DateTimeKind.Unspecified).AddTicks(2849), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Clientes",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 10, 12, 57, 14, 372, DateTimeKind.Unspecified).AddTicks(823), new TimeSpan(0, -3, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Clientes",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 10, 12, 57, 14, 372, DateTimeKind.Unspecified).AddTicks(871), new TimeSpan(0, -3, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Clientes",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created_at",
+                value: new DateTimeOffset(new DateTime(2026, 9, 10, 12, 57, 14, 372, DateTimeKind.Unspecified).AddTicks(882), new TimeSpan(0, -3, 0, 0, 0)));
+        }
+    }
+}
